adb1a24dba3a389a8c3fc551412e127ece30340d
Dwayne <dune.njenga@student.moringaschool.com>
2025-03-13 12:49:49 +0300

    Completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  form = document.querySelector('#create-task-form')
+  formInput = document.querySelector('#new-task-description')
+  taskList = document.querySelector('#tasks')
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildToDo(formInput.value);
+    form.reset();
+  })
 });
+
+function buildToDo(todo){
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', deleteTodo)
+  btn.textContent = "Delete"
+  li.textContent = `${todo} `;
+  li.appendChild(btn);
+  taskList.appendChild(li);
+}
+
+function deleteTodo(e){
+  e.target.parentNode.remove();
+}
